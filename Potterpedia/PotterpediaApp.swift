189a5d41e92109e749dbc5b7701cf8adde9a9897
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-11-04 20:20:18 -0700

    Initial Commit

---
+++ Potterpedia/PotterpediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PotterpediaApp.swift
+//  Potterpedia
+//
+//  Created by Kabir Dhillon on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct PotterpediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
