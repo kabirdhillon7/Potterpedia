f4a01f8319a33ba5315e948fccd6048dc5483f17
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-11-05 23:22:27 -0800

    Update CharacterView.swift
    Added error view

--- Potterpedia/Views/CharacterView.swift
+++ Potterpedia/Views/CharacterView.swift
@@ -16,30 +16,43 @@ struct CharacterView: View {
     
     var body: some View {
         NavigationStack {
-            ScrollView {
-                LazyVGrid(columns: columns, spacing: 0) {
-                    ForEach($viewModel.characters) { character in
-                        ZStack(alignment: .bottomLeading) {
-                            AsyncImage(url: URL(string: character.image.wrappedValue)) { image in
-                                image
-                                    .resizable()
-                                    .aspectRatio(1, contentMode: .fit)
-                            } placeholder: {
-                                Image(systemName: "person.fill")
-                                    .resizable()
-                                    .aspectRatio(1, contentMode: .fill)
+            if viewModel.characters.isEmpty && viewModel.error != nil {
+                VStack {
+                    Image(systemName: "person.crop.circle.badge.exclamationmark")
+                        .font(.system(size: 75))
+                        .foregroundColor(Color(UIColor.lightGray))
+                    Spacer()
+                        .frame(height: 5)
+                    Text("Error: Unable to Retrieve Harry Potter Characters")
+                        .fontWeight(.medium)
+                        .foregroundColor(Color(UIColor.lightGray))
+                }
+            } else {
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 0) {
+                        ForEach($viewModel.characters) { character in
+                            ZStack(alignment: .bottomLeading) {
+                                AsyncImage(url: URL(string: character.image.wrappedValue)) { image in
+                                    image
+                                        .resizable()
+                                        .aspectRatio(1, contentMode: .fit)
+                                } placeholder: {
+                                    Image(systemName: "person.fill")
+                                        .resizable()
+                                        .aspectRatio(1, contentMode: .fill)
+                                }
+                                LinearGradient(gradient: Gradient(colors: [.clear, .black]), startPoint: .center, endPoint: .bottom)
+                                Text(character.name.wrappedValue)
+                                    .foregroundStyle(.white)
+                                    .fontWeight(.medium)
+                                    .font(.system(size: 15))
+                                    .padding([.leading, .bottom], 5)
                             }
-                            LinearGradient(gradient: Gradient(colors: [.clear, .black]), startPoint: .center, endPoint: .bottom)
-                            Text(character.name.wrappedValue)
-                                .foregroundStyle(.white)
-                                .fontWeight(.medium)
-                                .font(.system(size: 15))
-                                .padding([.leading, .bottom], 5)
                         }
                     }
                 }
+                .navigationTitle("Characters")
             }
-            .navigationTitle("Characters")
         }
         .onAppear(perform: {
             viewModel.fetchCharacter()
